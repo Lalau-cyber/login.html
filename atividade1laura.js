468c6406a8eda2fa71af8ebdc45fd1d7b2f4e4fc
Laura <laurinha.ss1305@gmail.com>
2025-05-29 15:32:22 -0300

    Add files via upload
    atividade que o professor arthur deu

---
+++ atividade1laura.js
@@ -0,0 +1,8 @@
+let numA = Number( prompt("digite o primeiro numero: "))
+let numB = Number(prompt("digite o segundo numero: "))
+
+let soma = numA + numB
+
+if(soma> 10){
+    alert(soma)
+}
---
+++ atividade2laura.js
@@ -0,0 +1,7 @@
+let sA, nS 
+sA = parseFloat(prompt("digite seu saldo: "))
+
+if(sA < 500){
+nS = sA + sal* 0.15
+}
+alert("seu novo saldo é: "+nS)
