8079c2323a7a4b6006f0384f74d4fcfadbd0c63d
Laura <laurinha.ss1305@gmail.com>
2025-06-02 17:13:53 -0300

    Add files via upload
    fiz

---
+++ atividade13.js
@@ -0,0 +1,12 @@
+let  n, num, F=0
+num=parseFloat(prompt("digite um numero"))
+
+let contador = "calculo fatorial"
+for( ; n>=1; n--){
+    contador += n + "\n" 
+    
+    F+= n * (i-1)
+}
+
+
+alert(F)
